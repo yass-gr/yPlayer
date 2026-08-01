cb14bdee4340a881e75465344511a14b2d79f552
yassine grairi <grairiiyassine@gmail.com>
2026-08-01 12:22:21 +0100

    polish premium motion and design pass

--- app/src/main/java/com/yplayer/player/MediaItemMapper.kt
+++ app/src/main/java/com/yplayer/player/MediaItemMapper.kt
@@ -3,10 +3,13 @@ package com.yplayer.player
 import android.content.ContentUris
 import android.net.Uri
 import android.provider.MediaStore
+import androidx.annotation.OptIn
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
+import androidx.media3.common.util.UnstableApi
 import com.yplayer.data.model.Song
 
+@OptIn(UnstableApi::class)
 fun Song.toMediaItem(): MediaItem = MediaItem.Builder()
     .setMediaId(uri.toString())
     .setUri(uri)
@@ -20,6 +23,7 @@ fun Song.toMediaItem(): MediaItem = MediaItem.Builder()
     )
     .build()
 
+@OptIn(UnstableApi::class)
 fun MediaItem.toSong(): Song {
     val uri = Uri.parse(mediaId)
     val id = uri.lastPathSegment?.toLongOrNull() ?: 0L
